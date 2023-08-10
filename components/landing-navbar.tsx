d2972c553945a5d935bd37e0625cc7848dea7e23
chrishuang <mgs2bio@gmail.com>
2023-08-10 10:33:29 -0400

    fixed

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -3,7 +3,6 @@
 import { Montserrat } from "next/font/google";
 import Image from "next/image"
 import Link from "next/link"
-import { useAuth } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,4 +1,4 @@
-import { useUser } from "@clerk/nextjs";
+
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
