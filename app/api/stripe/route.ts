96d4dfaaefce2da5dd3fe1edad3fbd3f932a95d7
chrishuang <mgs2bio@gmail.com>
2023-08-10 10:23:29 -0400

    fixed cleker

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,4 +1,4 @@
-import { auth, currentUser } from "@clerk/nextjs";
+
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
@@ -9,8 +9,8 @@ const settingsUrl = absoluteUrl("/settings");
 
 export async function GET() {
   try {
-    const { userId } = auth();
-    const user = await currentUser();
+    const { userId } = {"userId" : "123"};
+
 
     if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -37,7 +37,7 @@ export async function GET() {
       payment_method_types: ["card"],
       mode: "subscription",
       billing_address_collection: "auto",
-      customer_email: user.emailAddresses[0].emailAddress,
+      customer_email: "abc@gmail.com",
       line_items: [
         {
           price_data: {
