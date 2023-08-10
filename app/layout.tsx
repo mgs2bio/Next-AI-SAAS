446c17daa540d2c02bac6db9ef54201153fd3a75
chrishuang <mgs2bio@gmail.com>
2023-08-10 10:31:03 -0400

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+
 
 import { ToasterProvider } from '@/components/toaster-provider'
 import { ModalProvider } from '@/components/modal-provider'
