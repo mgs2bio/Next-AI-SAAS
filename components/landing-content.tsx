e0e7310028d4778f675c91a5b197c8336eaefa86
chrishuang <mgs2bio@gmail.com>
2023-08-13 17:35:45 -0400

    fixed

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,25 +4,25 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Hilbert",
+    name: "DayDayYearYearDayDay",
     avatar: "J",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Hilbert",
+    name: "DayDayYearYearDayDay",
     avatar: "A",
     title: "Designer",
     description: "I use this daily for generating new photos!",
   },
   {
-    name: "Hilbert",
+    name: "DayDayYearYearDayDay",
     avatar: "M",
     title: "CEO",
     description: "This app has changed my life, cannot imagine working without it!",
   },
   {
-    name: "Hilbert",
+    name: "DayDayYearYearDayDay",
     avatar: "M",
     title: "CFO",
     description: "The best in class, definitely worth the premium subscription!",
